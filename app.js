bb825fcdd81c6f5f619c033cd11bd34bd5ab09a8
ChineduG <investorgbriel@gmail.com>
2022-06-21 10:09:43 +0100

    routes created

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+const tasks = require('./routes/task');
+
+// middleware 
+app.use(express.json());
+
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager Live')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+ 
+app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
---
+++ routes/task.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const 
+{ 
+    getAllTask, 
+    createTask, 
+    getTask, 
+    updateTask, 
+    deleteTask 
+} = require('../controllers/task')
+ 
+// routes 
+router.route('/').get(getAllTask).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+
+
+
+module.exports = router
